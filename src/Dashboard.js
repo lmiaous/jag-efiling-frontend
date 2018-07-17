5d3faddf8c760edcf5b2d7b67b105ad382107e41
perkinss <perkins.susan@gmail.com>
2018-07-16 17:27:16 -0700

    using iframe not image.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,6 +4,7 @@ import Top5 from './forms/Top5.js';
 import ActiveFormList from './components/ActiveFormList.js';
 import NeedHelp from './NeedHelp.js';
 import './dashboard.css';
+import Iframe from 'react-iframe'
 
 class Dashboard extends Component {
 
@@ -30,6 +31,20 @@ class Dashboard extends Component {
                                 <NeedHelp/>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col col-lg-12 form-section">
+
+                        <Iframe url="https://dev.justice.gov.bc.ca/cso/ext/coa/form7/#/qualify?_k=f90vwa"
+                                width="100%"
+                                height="800px"
+                                id="iframe-form7"
+                                className="form-7-iframe"
+                                display="initial"
+                                position="relative"
+                                target="_parent"
+                                allowFullScreen/>
                     </div>
                 </div>
             </div>
